d8f531e3e89776e81c9cf19a223df3ab3455e7d9
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-01-29 20:53:30 +0100

    implement imprint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
+import { ImprintComponent } from './shared/imprint/imprint.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, ImprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
 export class AppComponent {
   title = 'Portfolio';
 }
