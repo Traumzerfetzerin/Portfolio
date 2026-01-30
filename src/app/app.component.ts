ac7875a4f311ce2f2bee7cb0cf26d23da59172ba
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-01-30 20:34:42 +0100

    implement menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ImprintComponent } from './shared/imprint/imprint.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, ImprintComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, ImprintComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
