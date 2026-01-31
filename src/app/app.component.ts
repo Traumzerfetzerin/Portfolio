0b88224a63a069bcb0d482a408728763aebd97fb
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-01-31 16:35:58 +0100

    implement privacy policy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ImprintComponent } from './shared/imprint/imprint.component';
 import { MenuComponent } from './shared/menu/menu.component';
+import { PoliceComponent } from './shared/police/police.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, ImprintComponent, MenuComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, ImprintComponent, MenuComponent, PoliceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
