1b91e9294bfec3f983ee6a2680762339e45cf0c5
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-02 19:55:27 +0100

    optimise skills

--- src/app/content/skills/skills.component.ts
+++ src/app/content/skills/skills.component.ts
@@ -10,5 +10,16 @@ import { Component } from '@angular/core';
 
 
 export class SkillsComponent {
-
+  skills = [
+    { img: 'assets/img/skills/Angular.png', name: 'Angular' },
+    { img: 'assets/img/skills/TypeScript.png', name: 'TypeScript' },
+    { img: 'assets/img/skills/Javascript.png', name: 'JavaScript' },
+    { img: 'assets/img/skills/html.png', name: 'HTML' },
+    { img: 'assets/img/skills/css.png', name: 'CSS' },
+    { img: 'assets/img/skills/api.png', name: 'REST-API' },
+    { img: 'assets/img/skills/Firebase.png', name: 'Firebase' },
+    { img: 'assets/img/skills/git.png', name: 'Git' },
+    { img: 'assets/img/skills/MaterialDesign.png', name: 'Material Design' },
+    { img: 'assets/img/skills/Scrum.png', name: 'Scrum' }
+  ];
 }
