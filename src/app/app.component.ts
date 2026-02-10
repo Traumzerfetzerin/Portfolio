cb7f3459a8fe17daae2b5c338e1c154878a1c56f
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-10 20:15:51 +0100

    implement material angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+
 
 // shared
 import { FooterComponent } from './shared/footer/footer.component';
@@ -14,12 +16,13 @@ import { MeComponent } from './content/me/me.component';
 import { SkillsComponent } from './content/skills/skills.component';
 import { ReviewComponent } from './content/review/review.component';
 import { ContactComponent } from './content/contact/contact.component';
+import { ProjectsComponent } from './content/projects/projects.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, ImprintComponent, MenuComponent, PoliceComponent, HeroComponent, MeComponent, SkillsComponent, ReviewComponent, ContactComponent],
+  imports: [CommonModule, RouterOutlet, MatTabsModule, FooterComponent, ImprintComponent, MenuComponent, PoliceComponent, HeroComponent, MeComponent, SkillsComponent, ReviewComponent, ContactComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
