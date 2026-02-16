9e0a055fe32ebf5cd91d63988b0398273aca090e
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-16 20:25:33 +0100

    optimise hero section

--- src/app/content/hero/hero.component.ts
+++ src/app/content/hero/hero.component.ts
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
-
+  /**
+   * Scroll to the given element with the given id.
+   * @param {string} sectionId - The id of the element to scroll to.
+   */
+  scrollTo(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
