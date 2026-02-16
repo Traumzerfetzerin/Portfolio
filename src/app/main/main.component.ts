b962f8c4e0ace125dede0ea1dcebc6a70beef860
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-16 21:04:14 +0100

    optimise routes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,35 +1,29 @@
 import { Component } from '@angular/core';
-import { HeroComponent } from '../content/hero/hero.component';
-import { MenuComponent } from '../shared/menu/menu.component';
+import { HeroComponent } from '../content/hero/hero.component';;
 import { MeComponent } from '../content/me/me.component';
 import { SkillsComponent } from '../content/skills/skills.component';
 import { ProjectsComponent } from '../content/projects/projects.component';
 import { ReviewComponent } from '../content/review/review.component';
 import { ContactComponent } from '../content/contact/contact.component';
-import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
     HeroComponent,
-    MenuComponent,
     MeComponent,
     SkillsComponent,
     ProjectsComponent,
     ReviewComponent,
-    ContactComponent,
-    FooterComponent
+    ContactComponent
   ],
   template: `
     <app-hero id="hero"></app-hero>
-    <app-menu></app-menu>
     <app-me id="whyMe"></app-me>
     <app-skills id="skills"></app-skills>
     <app-projects id="projects"></app-projects>
     <app-review id="review"></app-review>
     <app-contact id="contact"></app-contact>
-    <app-footer></app-footer>
   `
 })
 export class MainComponent { }
