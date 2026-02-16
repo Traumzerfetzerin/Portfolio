6df09a426e4dce889734a0c227f509c6996a501f
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-16 21:15:12 +0100

    optimise routes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HeroComponent } from '../content/hero/hero.component';;
+import { HeroComponent } from '../content/hero/hero.component';
 import { MeComponent } from '../content/me/me.component';
 import { SkillsComponent } from '../content/skills/skills.component';
 import { ProjectsComponent } from '../content/projects/projects.component';
