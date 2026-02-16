b7340d1e8a13e9393e86bc946661e3b91eaae947
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-16 20:27:30 +0100

    scroll to contact from me section

--- src/app/content/me/me.component.ts
+++ src/app/content/me/me.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-me',
   standalone: true,
@@ -7,6 +8,16 @@ import { Component } from '@angular/core';
   templateUrl: './me.component.html',
   styleUrl: './me.component.scss'
 })
-export class MeComponent {
 
+export class MeComponent {
+  /**
+   * Scroll to the given element with the given id.
+   * @param {string} sectionId - The id of the element to scroll to.
+   */
+  scrollTo(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
