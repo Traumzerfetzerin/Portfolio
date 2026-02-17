e466a73f7bb987625e51e7ebb48b2b95dd6dfb20
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-17 21:00:03 +0100

    optimise menu ts

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -16,31 +16,6 @@ export class MenuComponent {
 
   constructor(private router: Router) { }
 
-  @HostListener('window:scroll', [])
-  /**
-   * Sets the active section to the section that is currently visible on the screen.
-   * The section is determined by the scroll position of the window.
-   * If the scroll position is within the bounds of a section, that section is considered active.
-   * The active section is updated every time the window is scrolled.
-   * @returns {void}
-   */
-
-  onWindowScroll() {
-    const scrollPos = window.scrollY + window.innerHeight / 3;
-
-    for (let sectionId of this.sections) {
-      const el = document.getElementById(sectionId);
-      if (!el) continue;
-
-      const top = el.offsetTop;
-      const bottom = top + el.offsetHeight;
-
-      if (scrollPos >= top && scrollPos < bottom) {
-        this.activeSection = sectionId;
-      }
-    }
-  }
-
 
   /**
    * Scroll to the given element with the given id.
