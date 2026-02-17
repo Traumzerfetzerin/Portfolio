f1527d0cb5e84a9e7ac501ecda3545d89d131926
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-17 20:58:31 +0100

    optimise menu ts

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-menu',
@@ -10,25 +11,46 @@ import { Router } from '@angular/router';
 
 
 export class MenuComponent {
+  activeSection: string = '';
+  sections: string[] = ['whyMe', 'skills', 'projects', 'contact'];
+
   constructor(private router: Router) { }
+
+  @HostListener('window:scroll', [])
+  /**
+   * Sets the active section to the section that is currently visible on the screen.
+   * The section is determined by the scroll position of the window.
+   * If the scroll position is within the bounds of a section, that section is considered active.
+   * The active section is updated every time the window is scrolled.
+   * @returns {void}
+   */
+
+  onWindowScroll() {
+    const scrollPos = window.scrollY + window.innerHeight / 3;
+
+    for (let sectionId of this.sections) {
+      const el = document.getElementById(sectionId);
+      if (!el) continue;
+
+      const top = el.offsetTop;
+      const bottom = top + el.offsetHeight;
+
+      if (scrollPos >= top && scrollPos < bottom) {
+        this.activeSection = sectionId;
+      }
+    }
+  }
+
 
   /**
    * Scroll to the given element with the given id.
-   * If the current router URL is not '/', navigate to '/' with the given sectionId as the fragment.
-   * Otherwise, get the element with the given id and scroll it into view with a smooth animation and block set to 'start'.
    * @param {string} sectionId - The id of the element to scroll to.
    */
   scrollTo(sectionId: string) {
-
-    if (this.router.url !== '/') {
-
-      this.router.navigate(['/'], { fragment: sectionId });
-    } else {
-
-      const element = document.getElementById(sectionId);
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      }
+    const el = document.getElementById(sectionId);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      this.activeSection = sectionId;
     }
   }
 }
