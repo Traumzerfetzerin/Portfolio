37e000b150fbf26d55f7e28b7c7e1109e490da13
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-24 21:00:58 +0100

    optimise routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,18 +2,11 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { ImprintComponent } from './shared/imprint/imprint.component';
 import { PoliceComponent } from './shared/police/police.component';
-import { LayoutComponent } from './layout/layout.component';
 
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: LayoutComponent,
-        children: [
-            { path: '', component: MainComponent },
-            { path: 'imprint', component: ImprintComponent },
-            { path: 'police', component: PoliceComponent }
-        ]
-    },
+    { path: '', component: MainComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'police', component: PoliceComponent },
     { path: '**', redirectTo: '' }
 ];
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../content/hero/hero.component';
-import { MenuComponent } from '../shared/menu/menu.component';
 import { MeComponent } from '../content/me/me.component';
 import { SkillsComponent } from '../content/skills/skills.component';
 import { ProjectsComponent } from '../content/projects/projects.component';
 import { ReviewComponent } from '../content/review/review.component';
 import { ContactComponent } from '../content/contact/contact.component';
+import { MenuComponent } from '../shared/menu/menu.component';
 
 
 @Component({
@@ -13,24 +13,27 @@ import { ContactComponent } from '../content/contact/contact.component';
   standalone: true,
   imports: [
     HeroComponent,
-    MenuComponent,
     MeComponent,
     SkillsComponent,
     ProjectsComponent,
     ReviewComponent,
-    ContactComponent
+    ContactComponent,
+    MenuComponent
   ],
+
+
   template: `
     <app-hero id="hero"></app-hero>
-    <app-menu id="menu"></app-menu>
-    <div class="gap">
+    <app-menu></app-menu>
     <app-me id="whyMe"></app-me>
     <app-skills id="skills"></app-skills>
     <app-projects id="projects"></app-projects>
     <app-review id="review"></app-review>
     <app-contact id="contact"></app-contact>
-    </div>
   `,
+
   styleUrls: ['./main.component.scss']
 })
+
+
 export class MainComponent { }
