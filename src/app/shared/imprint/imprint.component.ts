68e956d2c9eb01337aa9462075f14eb826bd6f2c
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-26 21:35:36 +0100

    optimise imprint

--- src/app/shared/imprint/imprint.component.ts
+++ src/app/shared/imprint/imprint.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
 
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
-
 
 export class ImprintComponent {
 
