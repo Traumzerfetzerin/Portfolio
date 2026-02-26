a4589339c8fcbae71b191656763c17ac5d25aaae
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-26 21:37:57 +0100

    optimise policy

--- src/app/shared/police/police.component.ts
+++ src/app/shared/police/police.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
 
 
 @Component({
   selector: 'app-police',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './police.component.html',
   styleUrl: './police.component.scss'
 })
