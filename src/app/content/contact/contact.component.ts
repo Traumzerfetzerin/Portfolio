f734a1b414ebafb27681b9d697bdbb61711e0555
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-02-28 18:36:10 +0100

    optimise contact me

--- src/app/content/contact/contact.component.ts
+++ src/app/content/contact/contact.component.ts
@@ -1,17 +1,53 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 
 
 export class ContactComponent {
+  contactForm: FormGroup;
+
+
+  /**
+   * Constructor
+   * @param {FormBuilder} fb - The form builder used to create the contact form.
+   */
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required],
+      acceptPolicy: [false, Validators.requiredTrue]
+    });
+  }
+
+
+  /**
+   * Handles the submission of the contact form.
+   * If the form is valid, logs the submitted data to the console,
+   * displays an alert to the user, and resets the form.
+   * If the form is invalid, marks all form controls as touched.
+   */
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log(this.contactForm.value);
+      alert('Message sent!');
+      this.contactForm.reset();
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
+
+
   /**
    * Scroll to the given element with the given id.
    * @param {string} sectionId - The id of the element to scroll to.
