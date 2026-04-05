c94ee960b8bad6a784ee8ddec84c4e4b83b62d94
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-04-05 21:34:07 +0200

    feat: add translate import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideTranslateService } from '@ngx-translate/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +14,8 @@ export const appConfig: ApplicationConfig = {
         scrollPositionRestoration: 'enabled'
       })
     ),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    provideTranslateService()
   ]
 };
