1611b80f30076f760e043b153b50699ba2b00e0c
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-04-05 22:19:00 +0200

    feat: add translate in me component ts

--- src/app/content/me/me.component.ts
+++ src/app/content/me/me.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-me',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './me.component.html',
   styleUrl: './me.component.scss'
 })
 
 export class MeComponent {
+
+  constructor(
+    private router: Router,
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+  }
+  
+
+  switchLang(lang: string) {
+    this.translate.use(lang);
+    console.log('TEST:', this.translate.instant('menu.skills'));
+  }
+
+
   /**
    * Scroll to the given element with the given id.
    * @param {string} sectionId - The id of the element to scroll to.
