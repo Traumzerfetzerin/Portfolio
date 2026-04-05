b99e220448af380fa66e8ae16123667d805f8a9b
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-04-05 22:13:47 +0200

    fix: translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, HttpClient } from '@angular/common/http';
-
-import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+
+/**
+ * Returns a new instance of TranslateHttpLoader which is responsible for loading translations
+ * from '/assets/i18n/' directory with '.json' extension.
+ * @param {HttpClient} http - The HttpClient which will be used to load translations.
+ * @returns {TranslateHttpLoader} A new instance of TranslateHttpLoader.
+ */
 export function httpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
 }
 
 export const appConfig: ApplicationConfig = {
@@ -23,12 +29,14 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideHttpClient(),
 
-    {
-      provide: TranslateLoader,
-      useFactory: httpLoaderFactory,
-      deps: [HttpClient]
-    },
-    
-    provideTranslateService()
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient]
+        }
+      })
+    )
   ]
 };
