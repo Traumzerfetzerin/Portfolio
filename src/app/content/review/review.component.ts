497ec2f3fc6d64111b2be511bff00b3f0ac92d05
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-04-06 12:46:45 +0200

    feat: import translate review

--- src/app/content/review/review.component.ts
+++ src/app/content/review/review.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './review.component.html',
   styleUrl: './review.component.scss'
 })
 
 
 export class ReviewComponent {
+  constructor(
+    private router: Router,
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+  }
+
+
+  switchLang(lang: string) {
+    this.translate.use(lang);
+  }
+
+
   reviews = [
     {
       name: 'Name',
