f42f206721301e30c06187877cd40342fc002165
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-04-06 12:21:48 +0200

    feat: import translate projects

--- src/app/content/projects/projects.component.ts
+++ src/app/content/projects/projects.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, TranslateModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
 
+
+export class ProjectsComponent {
+  constructor(
+    private router: Router,
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+  }
+
+
+  switchLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
