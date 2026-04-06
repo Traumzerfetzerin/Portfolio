3ecd30ae1615878daa287082f8c8d028f3c4d03f
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-04-06 12:47:29 +0200

    docs: add JSDoc to the functions

--- src/app/content/review/review.component.ts
+++ src/app/content/review/review.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslateModule } from '@ngx-translate/core';
 
+
 @Component({
   selector: 'app-review',
   standalone: true,
@@ -13,6 +14,12 @@ import { TranslateModule } from '@ngx-translate/core';
 
 
 export class ReviewComponent {
+  /**
+   * Constructor for the ReviewComponent.
+   * Sets the default language for the TranslateService to 'de' and uses it.
+   * @param {Router} router - The Angular Router for navigating to different routes.
+   * @param {TranslateService} translate - The TranslateService for translating text.
+   */
   constructor(
     private router: Router,
     private translate: TranslateService
@@ -22,6 +29,12 @@ export class ReviewComponent {
   }
 
 
+  /**
+   * Switches the language of the application to the given language.
+   * @param {string} lang - The language to switch to.
+   * @example
+   * switchLang('en')
+   */
   switchLang(lang: string) {
     this.translate.use(lang);
   }
