e0478430ba6d73e8a1a2697f88ff0ed7f44f3b1b
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-04-06 14:08:35 +0200

    feat: import translate police

--- src/app/shared/police/police.component.ts
+++ src/app/shared/police/police.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-police',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, TranslateModule],
   templateUrl: './police.component.html',
   styleUrl: './police.component.scss'
 })
 
 
 export class PoliceComponent {
+  constructor(
+    private router: Router,
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+  }
 
+
+  switchLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
