99ff0356647b3e4fe1972ba6d93730e4abcc9615
Traumzerfetzerin <168225733+Traumzerfetzerin@users.noreply.github.com>
2026-05-07 21:00:37 +0200

    style: remove language switcher in why me

--- src/app/content/me/me.component.ts
+++ src/app/content/me/me.component.ts
@@ -2,41 +2,21 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { LanguageSwitcherComponent } from '../../shared/language-switcher/language-switcher.component';
 
 
 @Component({
   selector: 'app-me',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, LanguageSwitcherComponent],
   templateUrl: './me.component.html',
   styleUrl: './me.component.scss'
 })
 
+
 export class MeComponent {
-  /**
-   * Constructor for the MeComponent.
-   * Sets the default language for the TranslateService to 'de' and uses it.
-   * @param {Router} router - The Angular Router for navigating to different routes.
-   * @param {TranslateService} translate - The TranslateService for translating text.
-   */
-  constructor(
-    private router: Router,
-    private translate: TranslateService
-  ) {
-    this.translate.setDefaultLang('de');
-    this.translate.use('de');
-  }
 
-
-  /**
-   * Switches the language of the application to the given language.
-   * @param {string} lang - The language to switch to.
-   * @example
-   * switchLang('en')
-   */
-  switchLang(lang: string) {
-    this.translate.use(lang);
-  }
+  constructor(private router: Router) { }
 
 
   /**
